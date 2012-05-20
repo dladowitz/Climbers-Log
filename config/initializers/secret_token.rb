03f7f9ef593cd0782c14638e0088f0b8cb0bb724
David Ladowitz <David@ladowitz.com>
2012-05-20 05:20:24 -0700

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClimbersLog::Application.config.secret_token = '94528646924124b0d84fb3178ec497f620eee952e6d6cf71f8d46fbc069134bc0beda9c6b611361452c74bab107b4520674bed07586a9b398f8947b97d10e96e'
